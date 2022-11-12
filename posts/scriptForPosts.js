9f47f18c61788544b8458c51820b0a3fc6d002f6
Lukasnvc <lukasnvc@gmail.com>
2022-11-12 15:26:10 +0200

    first commit

---
+++ posts/scriptForPosts.js
@@ -0,0 +1,123 @@
+const postForm = document.querySelector('#newpost');
+const close = document.querySelector('#close');
+const newPost = document.querySelector('#newPost');
+const postBtn = document.querySelector('#submit');
+const newPostBtn = document.querySelector('#newPostBtn');
+const h2 = document.querySelector('#h2');
+const logout = document.querySelector('#logout');
+const container = document.querySelector('#posts');
+
+logout.addEventListener('click', () => {
+  localStorage.clear();
+  location.href = '/../index.html';
+})
+
+newPostBtn.addEventListener('click', () => {
+  postForm.style.display = 'block';
+  newPostBtn.style.display = 'none';
+})
+
+close.addEventListener('click', () => {
+  postForm.style.display = 'none';
+  newPostBtn.style.display = 'block';
+})
+
+
+
+const getData = () => {
+  fetch('https://testapi.io/api/lukasnvc/resource/NotesProject',
+{
+  method: 'GET',
+  headers: {
+    'Content-Type':
+    'application/json'
+  }
+})
+.then((response) => {
+  if (response.ok) {
+    return response.json()
+  }
+})
+.then((result) => {
+  console.log('Data got from GET: ', result.data)
+  return result.data
+})
+.then((data)=>{
+  console.log('Type or data getting: ', typeof data)
+  filter(data)
+})
+}
+
+getData()
+
+const sendUppdatedPosts = (name, id, posts) => {
+fetch(`https://testapi.io/api/lukasnvc/resource/NotesProject/${id}`,
+{
+  method: 'PUT',
+  headers: {
+    'Content-Type':
+    'application/json'
+  },
+  body: JSON.stringify({
+    username: `${name}`,
+    post: `${posts}`
+  }) 
+})
+.then((response) => {
+  if (response.ok) {
+    return response.json()
+  }
+})
+.then((result) => {
+  console.log('Fetching data : ', result);
+  container.replaceChildren()
+  getData()
+})
+}
+
+const arr = JSON.parse(localStorage.getItem('user'));
+const lastIndex = arr.length;
+const id = arr[lastIndex-1];
+console.log('User id :', id)
+
+
+const filter = (users) => {
+  let user
+users.forEach(element => {
+  if(element.id==id) {
+    user=element
+    console.log('Found user posts by id :',user);
+    draw(user);
+    makePost(user)
+  }
+});
+}
+
+const draw = (user) => {
+  const userPostsArr = JSON.parse(user.post) || []
+  userPostsArr.forEach(element => {
+    console.log('Drawing in forEach : ',element)
+    const div = document.createElement('div');
+    div.setAttribute('class', 'card');
+    div.textContent=element;
+    container.appendChild(div)
+    
+  });
+
+}
+
+const makePost = (item) => {
+postBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const allPosts = item.post;
+  
+  const currentPost = newPost.value;
+
+  const postsArr = JSON.parse(item.post) || []
+  console.log('User posts in makePost :', allPosts, typeof allPosts, ', New post :', currentPost);
+  postsArr.push(currentPost);
+  const jsonPostsArr = JSON.stringify(postsArr)
+  sendUppdatedPosts(item.username, id, jsonPostsArr)
+  postForm.style.display = 'none';
+})
+}
---
+++ scriptForIndex.js
@@ -0,0 +1,122 @@
+const loginUsername = document.querySelector('#usernameLogin');
+const loginPassword = document.querySelector('#passwordLogin');
+const loginBtn = document.querySelector('#submit1');
+const login = document.querySelector('#login');
+const error = document.querySelector('#error');
+
+// Register
+const registerBtn = document.querySelector('#registerBtn');
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
+const password2 = document.querySelector('#password2');
+const submit = document.querySelector('#submit');
+const close = document.querySelector('#close');
+const registerForm = document.querySelector('#register');
+const h3 = document.querySelector('#registerH3');
+
+//login
+  const getUser = () => {
+   fetch('https://testapi.io/api/lukasnvc/resource/NotesProject',
+   {
+    method: 'GET',
+    headers: {
+    'Content-Type':
+    'application/json'
+    }
+    })
+   
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      }
+    })  
+     .then(data => {
+      console.log(data)
+      return data.data
+    }).then(users => {
+      console.log(users)
+      checkUser(users)
+    } )
+  }
+  
+  getUser()
+
+ 
+
+  const checkUser = (data) => {
+  loginBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const user = loginUsername.value+loginPassword.value;
+    data.forEach(element => {
+      console.log(element.username)
+      if (element.username===user){
+        console.log(element)
+        pushUser(element);
+        location.href = 'posts/posts.html';
+      } else {
+        loginPassword.value='';
+        error.textContent= "Wrong password or no such user"
+      }
+    })
+  })
+  }
+
+  const pushUser = (user) => {
+    
+    let arr=JSON.parse(localStorage.getItem('user')) || [];
+    arr.push(user.id);
+
+    console.log(user, arr)
+    localStorage.setItem('user', JSON.stringify(arr));
+  }
+//login
+
+//register
+registerBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  registerForm.style.display= 'block';
+})
+
+close.addEventListener('click', (e) => {
+  e.preventDefault()
+  registerForm.style.display= 'none';
+})
+
+const register = (valueUsername) => {
+  fetch('https://testapi.io/api/lukasnvc/resource/NotesProject',
+{
+  method: 'POST',
+  headers: {
+    'Content-Type':
+    'application/json'
+  },
+  body: JSON.stringify({
+    username: `${valueUsername}`
+  }) 
+})
+.then((response) => {
+  if (response.ok) {
+    return response.json()
+  }
+})
+.then((result) => {
+  console.log(result);
+  location.reload()
+})
+}
+
+  submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    let valueUsername
+    if (password.value===password2.value) {
+    valueUsername= username.value + password.value;
+    console.log(valueUsername);
+    register(valueUsername)
+    registerForm.style.display= 'none';
+  } else {
+    h3.textContent="Password don't match";
+    h3.style.color= "red";
+    loginPassword.value='';
+  }
+  })
